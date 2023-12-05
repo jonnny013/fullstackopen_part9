bd3ee3e34833475c9da3a4435b4a376225d24793
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-12-05 09:56:22 +0800

    set up exercise file

---
+++ tsExercises/bmiCalculator.ts
@@ -0,0 +1,11 @@
+const calculateBmi = (height: number, weight: number): string => {
+  //some calculations
+  let result
+  switch (result) {
+    case 1:
+      return 'normal (healthy weight)'
+  }
+}
+
+
+console.log(calculateBmi(180, 74))
