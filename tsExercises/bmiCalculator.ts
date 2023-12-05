237add67ad6bce63da5dacbc5856e5294127239f
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-12-05 10:14:20 +0800

    Finish 9.1

--- tsExercises/bmiCalculator.ts
+++ tsExercises/bmiCalculator.ts
@@ -1,9 +1,16 @@
 const calculateBmi = (height: number, weight: number): string => {
-  //some calculations
-  let result
-  switch (result) {
-    case 1:
-      return 'normal (healthy weight)'
+  let heightInMetres = height / 100
+  let result = weight/(heightInMetres * heightInMetres)
+  if (result < 18.5) {
+    return 'underweight'
+  } else if (result >= 18.5 && result < 25) {
+    return 'normal (healthy weight)'
+  } else if (result >= 25 && result < 30) {
+    return 'overweight'
+  } else if (result > 30) {
+    return 'obese'
+  } else {
+    throw new Error('Weight and height not given')
   }
 }
 
