7c8b09b77205ad64eb80edfba616b4b2efdf2a9c
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-12-12 10:50:04 +0800

    Add route and service and type for diagnoses move index to src directory update package.json for change of index location

---
+++ backend_patientor/src/services/diagnosesService.ts
@@ -0,0 +1,16 @@
+import diagnoses from '../../data/diagnoses';
+
+import { Diagnoses } from '../types';
+
+const getDiagnoses = (): Diagnoses[] => {
+  return diagnoses;
+};
+
+const addDiagnoses = () => {
+  return null;
+};
+
+export default {
+  getDiagnoses,
+  addDiagnoses,
+};
