e8a4076193ffd73614362487e3092fd1ce3428bb
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-12-20 10:41:52 +0800

    Finish 9.23 and 9.24
    Add entries to FE
    Add services for getting diagnosis info
    Add function to get diagnosis in IndividualPatient
    Add display for diagnosis based on code

--- patientor/src/components/PatientListPage/IndividualPatient.tsx
+++ patientor/src/components/PatientListPage/IndividualPatient.tsx
@@ -1,24 +1,29 @@
 import {useEffect, useState} from 'react';
 import patientService from '../../services/patients';
+import diagnosisService from '../../services/diagnosis';
 import {Skeleton, CardContent, Card, Typography} from '@mui/material';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
-import { Patient } from '../../types';
+import { Diagnosis, Patient } from '../../types';
 import { useParams } from 'react-router-dom';
 import TransgenderIcon from '@mui/icons-material/Transgender';
+import { Entry } from '../../types';
 
 const IndividualPatient = () => {
   const [patient, setPatient] = useState<Patient | undefined>(undefined);
+  const [ diagnosis, setDiagnosis ] = useState<Diagnosis[] | undefined>(undefined);
   const id = useParams().id;
-  console.log(id);
+ 
   useEffect(() => {
-    const getPatient = async () => {
+    const getData = async () => {
       if (id) {
         const data = await patientService.getIndividualPatient(id);
         setPatient(data);
       }
+      const data = await diagnosisService.getAll();
+      setDiagnosis(data);
     };
-    void getPatient();
+    void getData();
   }, []);
 
   const getGenderIcon = (gender: string) => {
@@ -40,6 +45,9 @@ const IndividualPatient = () => {
         <Skeleton animation='wave' width={350} height={70} />
         <Skeleton animation='wave' width={300} height={35} />
         <Skeleton animation='wave' width={300} height={35} />
+        <Skeleton animation='wave' width={150} height={70} />
+        <Skeleton animation='wave' width={300} height={35} />
+        <Skeleton animation='wave' width={300} height={35} />
       </Card>
     );
   }
@@ -54,6 +62,30 @@ const IndividualPatient = () => {
           </Typography>
           <Typography>SSN: {patient.ssn}</Typography>
           <Typography>Occupation: {patient.occupation}</Typography>
+          <br />
+          <Typography variant='h4'>Entries</Typography>
+          {patient.entries &&
+            patient.entries.map((entry: Entry) => (
+              <div key={entry.id}>
+                <Typography>
+                  {entry.date}: {entry.description}{' '}
+                </Typography>
+                <ul>
+                  {entry.diagnosisCodes &&
+                    entry.diagnosisCodes.map(code => {
+                      const matchingDiagnosis = diagnosis?.find(d => d.code === code);
+                      return (
+                        <li key={code}>
+                          {code}:{' '}
+                          {matchingDiagnosis
+                            ? matchingDiagnosis.name
+                            : 'Unknown Diagnosis'}
+                        </li>
+                      );
+                    })}
+                </ul>
+              </div>
+            ))}
         </CardContent>
       </Card>
     </>
